916c05ad49e69f305f0a44b61d35f0ad480b3faa
cheeric <eric10759@gmail.com>
2012-06-04 18:57:55 -0400

    Fixed shift-middle camera move. Can select faces after camera is moved.
    Can only rotate around world origin still.

--- js/camera.js
+++ js/camera.js
@@ -4,11 +4,10 @@ function Camera(eyeX, eyeY, eyeZ, refX, refY, refZ) {
 	this.upVec = vec3.create([0.0, 1.0, 0.0]);
 	this.PR = vec3.create([eyeX, eyeY, eyeZ]);
 	this.REF = vec3.create([refX, refY, refZ]);
+	this.rotREF = vec3.create([0, 0, 0]);
 
 	this.camXAngle = 0.0;
 	this.camYAngle = 0.0;
-
-	this.view = this.calcView(vec3.add(this.PR, this.REF));
 
 	this.fov = 60.0;
 }
@@ -18,56 +17,51 @@ Camera.prototype.setWindowSize = function(width, height){
 	this.windowHeight = height;
 }
 
-Camera.prototype.calcView = function(viewVec) {
-	/*// G = UP - N ( N . UP)
-	var G = vec3.subtract(this.upVec, vec3.scale(this.N, vec3.dot(this.N, this.upVec)));
-
-	// V = G / || G ||
-	var V = vec3.normalize(G);
-
-	// U = -N x V
-	var U = vec3.cross(vec3.negate(this.N), V);
-
-	// calculate rotation matrix
-	var R = mat4.create([U[0], V[0], this.N[0], 0.0,
-				  U[1], V[1], this.N[1], 0.0,
-				  U[2], V[2], this.N[2], 0.0,
-				  0.0, 0.0, 0.0, 1.0]); 
-	*/
-
-	// calculate translation matrix
-	this.T = mat4.create();
-	mat4.identity(this.T);
-	mat4.translate(this.T, vec3.negate(viewVec, vec3.create()));
-	
-	//alert(mat4.str(T));
-	return this.T;
-}
-
 Camera.prototype.getVMatrix = function() {
-	var view_rot = mat4.create();
-	mat4.rotate(this.view, this.camYAngle, vec3.create([1, 0, 0]), view_rot);
-	view_rot = mat4.rotate(view_rot, this.camXAngle, vec3.create([0, 1, 0]));
+	
+	// calculate translation matrix
+	this.T1 = mat4.create();
+	mat4.identity(this.T1);
+	this.T1[12] = -this.rotREF[0];
+	this.T1[13] = -this.rotREF[1];
+	this.T1[14] = -this.rotREF[2];
 
-	//this.aspect = this.windowWidth/this.windowHeight;
-	//var proj = mat4.perspective(this.fov, this.aspect, 0.1, 100.0);
-	return view_rot;
+	this.R = mat4.create();
+	mat4.identity(this.R);
+	mat4.rotateX(this.R, degToRad(this.camYAngle));
+	mat4.rotateY(this.R, degToRad(this.camXAngle));
+
+	this.T2 =  mat4.create();
+	mat4.identity(this.T2);
+	this.T2[12] = -(this.REF[0] - this.rotREF[0] + this.PR[0]);
+	this.T2[13] = -(this.REF[1] - this.rotREF[1] + this.PR[1]);
+	this.T2[14] = -(this.REF[2] - this.rotREF[2] + this.PR[2]);
+
+	this.view = mat4.create();
+	mat4.identity(this.view);
+	mat4.multiply(this.T1, this.view, this.view);
+	mat4.multiply(this.R, this.view, this.view);
+    mat4.multiply(this.T2, this.view, this.view);
+
+	return this.view;
 }
 
 Camera.prototype.getEyeVec = function() {
 	var model = mat4.create();
 	mat4.identity(model);
-	mat4.rotate(model, -this.camXAngle, [0, 1, 0]);
-	mat4.rotate(model, -this.camYAngle, [1, 0, 0]);
-	var temp = mat4.multiplyVec4(model, [this.REF[0]+this.PR[0], this.REF[1]+this.PR[1], this.REF[2]+this.PR[2], 1.0]);
+	mat4.rotateY(model, degToRad(-this.camXAngle));
+	mat4.rotateX(model, degToRad(-this.camYAngle));
+
+	var temp = mat4.multiplyVec4(model, [this.REF[0]-this.rotREF[0]+this.PR[0], this.REF[1]-this.rotREF[1]+this.PR[1], this.REF[2]-this.rotREF[2]+this.PR[2], 1.0]);
 	return [temp[0], temp[1], temp[2]];
 }
 
 Camera.prototype.getUpVec = function() {
 	var model = mat4.create();
 	mat4.identity(model);
-	mat4.rotate(model, -this.camXAngle, [0, 1, 0]);
-	mat4.rotate(model, -this.camYAngle, [1, 0, 0]);
+	mat4.rotateY(model, degToRad(-this.camXAngle));
+	mat4.rotateX(model, degToRad(-this.camYAngle));
+
 	var temp = mat4.multiplyVec4(model, [this.upVec[0], this.upVec[1], this.upVec[2], 1.0]);
 	return [temp[0], temp[1], temp[2]];
 }
@@ -79,11 +73,11 @@ Camera.prototype.getRef = function() {
 
 
 Camera.prototype.rotUp = function(angle) {
-	this.camYAngle += angle;
+	this.camYAngle -= angle;
 }
 
 Camera.prototype.rotDown = function(angle) {
-	this.camYAngle -= angle;
+	this.camYAngle += angle;
 }
 
 Camera.prototype.rotRight = function(angle) {
@@ -97,33 +91,90 @@ Camera.prototype.rotLeft = function(angle) {
 
 Camera.prototype.zoomIn = function() {
 	this.PR[2] *= 9.0/10.0;
-	this.view = this.calcView(vec3.add(this.PR, this.REF));
 }
 
 Camera.prototype.zoomOut = function() {
 	this.PR[2] /= 9.0/10.0;
-	this.view = this.calcView(vec3.add(this.PR, this.REF));
 }
 
 
 Camera.prototype.moveUp = function(dist) {
-	this.PR[1] += dist/110;
-	this.view = this.calcView(vec3.add(this.PR, this.REF));
+	var A = vec3.create(), B = vec3.create(), C = vec3.create();
+
+    var E = cam.getEyeVec();
+    var U = cam.getUpVec();
+    var center = cam.REF;
+    vec3.subtract(center, E, C);
+    vec3.cross(C, U, A);
+    vec3.cross(A, C, B);
+    vec3.normalize(A);
+    alert(vec3.str(A));
+	
+	this.PR[1] -= dist/110;
+	this.REF[1] -= dist/110;
 }
 
 Camera.prototype.moveDown = function(dist) {
-	this.PR[1] -= dist/110;
-	this.view = this.calcView(vec3.add(this.PR, this.REF));
+	this.PR[1] += dist/110;
+	this.REF[1] += dist/110;
 }
 
 Camera.prototype.moveRight = function(dist) {
-	this.PR[0] += dist/110;
-	this.view = this.calcView(vec3.add(this.PR, this.REF));
+	this.PR[0] -= dist/110;
+	this.REF[0] -= dist/110;
 }
 
 Camera.prototype.moveLeft = function(dist) {
-	this.PR[0] -= dist/110;
-	this.view = this.calcView(vec3.add(this.PR, this.REF));
+	this.PR[0] += dist/110;
+	this.REF[0] += dist/110;
+}
+
+
+Camera.prototype.move = function(T) {
+	vec3.negate(T);
+
+	var model = mat4.create();
+	mat4.identity(model);
+	mat4.rotateY(model, degToRad(-this.camXAngle));
+	mat4.rotateX(model, degToRad(-this.camYAngle));
+	mat4.inverse(model);
+	
+	var T4 = mat4.multiplyVec4(model, [T[0], T[1], T[2], 1]);
+
+	T[0] = T4[0];
+	T[1] = T4[1];
+	T[2] = T4[2];
+
+	this.REF[0] += vec3.dot(T, [1,0,0]);
+	this.REF[1] += vec3.dot(T, [0,1,0]);
+}
+
+Camera.prototype.getP = function(x, y) {
+	var n_x = x / canvas.width;
+    var n_y = 1 - (y / canvas.height);
+
+    var A = vec3.create(), B = vec3.create(), C = vec3.create(), M = vec3.create(), V = vec3.create(), H = vec3.create(), P = vec3.create();
+
+    var E = vec3.create();
+    vec3.add(this.REF, this.PR, E);
+    var U = cam.upVec;
+    var center = cam.REF;
+    vec3.subtract(center, E, C);
+    vec3.cross(C, U, A);
+    vec3.cross(A, C, B);
+    vec3.add(E,C,M);
+    var phi = degToRad(fov/2);
+    vec3.scale(B, (1.0/(vec3.length(B)) * (vec3.length(C)*Math.tan(phi))), V);
+    vec3.scale(A, (1.0/(vec3.length(A)) * (vec3.length(C)*Math.tan(phi)*(canvas.width/canvas.height))), H);
+    vec3.add(vec3.add(M, vec3.scale(H, (2*n_x-1))), vec3.scale(V, (2*n_y-1)), P);
+
+    var model = mat4.create();
+	mat4.identity(model);
+	mat4.rotateY(model, degToRad(-this.camXAngle));
+	mat4.rotateX(model, degToRad(-this.camYAngle));
+
+	var temp = mat4.multiplyVec4(model, [P[0], P[1], P[2], 1.0]);
+	return [temp[0], temp[1], temp[2]];
 }
 
 
